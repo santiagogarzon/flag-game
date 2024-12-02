e396dd033b7e3078d56cf6ab094167a5fd45b55a
Santiago Garzon <santiago.garzon@tarmac.io>
2024-12-02 15:48:49 +0200

    upgrading sdk

--- app/hooks/useGameManager.tsx
+++ app/hooks/useGameManager.tsx
@@ -48,7 +48,7 @@ export const getFlagId = (flag: Flag) =>
   kebabCase(toLower(flag.country)) as keyof typeof flagsColorInfo;
 
 export const useGameManager = () => {
-  const testing = false; // useAtomValue(testerModeActiveAtom);
+  const testing = true; // useAtomValue(testerModeActiveAtom);
   const [lastLiveUpdate, setLastLivesUpdate] = useAtom(lastLivesUpdateAtom);
   const [lives, setLives] = useAtom(livesAtom);
   const [completedFlags, setCompletedFlags] = useAtom(completedFlagsAtom);
